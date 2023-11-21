402b484131e4700699a0b8ae5c28a97aa9936aa1
kasahara-t <kasahara.tomoya.dev@gmail.com>
2023-11-21 15:36:32 +0900

    feat: :zap: ルートコンポーネントにナビゲーションとメインコンテンツを追加

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 
 export const Root = () => {
   return (
-    <div>
-      <Outlet />
+    <div className="flex absolute top-0 right-0 left-0 bottom-0">
+      <nav className="w-1/12">
+        <ul>
+          <li>
+            <NavLink className="block" to="/id-generator">
+              Id Generator
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <main className="flex-grow">
+        <div className="relative w-full h-full">
+          <Outlet />
+        </div>
+      </main>
     </div>
   );
 };
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
-import { createBrowserRouter } from 'react-router-dom';
+import { Navigate, createBrowserRouter } from 'react-router-dom';
 import { Root } from './Root';
 import ErrorPage from './ErrorPage';
+import { IdGeneratorPresenter } from '../features/id-generator/presenters/IdGeneratorPresenter';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Navigate to="/id-generator" replace />,
+      },
+      {
+        path: '/id-generator',
+        element: <IdGeneratorPresenter />,
+      },
+    ],
   },
 ]);
