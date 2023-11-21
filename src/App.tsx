8a7be34dcaa586993f43c9f5ed002fe95b12b42b
kasahara-t <kasahara.tomoya.dev@gmail.com>
2023-11-21 12:49:43 +0900

    feat: :zap: エラーページを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Root } from './pages/Root';
+import ErrorPage from './pages/ErrorPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
