3a39c95e37139af45388759bfee7c5ba17e87877
kasahara-t <kasahara.tomoya.dev@gmail.com>
2023-12-21 20:34:05 +0900

    feat: 💄 タブを別コンポーネントに切り出し

--- src/features/id-generator/components/IdGeneratorPresenter.tsx
+++ src/features/id-generator/components/IdGeneratorPresenter.tsx
@@ -1,6 +1,5 @@
 import { FC, ReactNode } from 'react';
-import { Tab } from '@headlessui/react';
-import { classNames } from '@common/utils/utils';
+import { TabView } from '@features/shared/ui/layout/TabView';
 import { ID } from '../models/ID';
 import { UUIDGenerateHandler } from '../models/UUID';
 import { IDHistoryPanel } from './IDHistoryPanel';
@@ -32,39 +31,7 @@ export const IdGeneratorPresenter: FC<IdGeneratorPresenterProps> = (props) => {
 
   return (
     <article>
-      <Tab.Group>
-        <Tab.List className="tw-flex tw-gap-x-1 tw-rounded-lg tw-bg-blue-900/20 tw-p-1">
-          {tabs.map((tab) => (
-            <Tab
-              key={tab.label}
-              className={({ selected }) =>
-                classNames(
-                  'tw-w-full tw-rounded-lg tw-py-2.5 tw-text-sm tw-font-medium tw-leading-5',
-                  'tw-ring-white/60 tw-ring-offset-2 tw-ring-offset-blue-400 tw-focus:outline-none tw-focus:ring-2',
-                  selected
-                    ? 'tw-bg-white tw-text-gray-700 tw-shadow'
-                    : 'tw-text-gray-800/40 tw-hover:bg-white/[0.12] tw-hover:text-white',
-                )
-              }
-            >
-              {tab.label}
-            </Tab>
-          ))}
-        </Tab.List>
-        <Tab.Panels className={'tw-mt-3'}>
-          {tabs.map((tab) => (
-            <Tab.Panel
-              key={tab.label}
-              className={classNames(
-                'tw-rounded-xl tw-border tw-p-3',
-                'tw-ring-white/60 tw-ring-offset-2 tw-ring-offset-blue-400 tw-focus:outline-none tw-focus:ring-2',
-              )}
-            >
-              {tab.panel}
-            </Tab.Panel>
-          ))}
-        </Tab.Panels>
-      </Tab.Group>
+      <TabView tabs={tabs} />
       <IDHistoryPanel ids={props.ids} />
     </article>
   );
---
+++ src/features/shared/ui/layout/TabView.tsx
@@ -0,0 +1,33 @@
+import { FC, ReactNode } from 'react';
+import { Tab } from '@headlessui/react';
+import { classNames } from '@common/utils/utils';
+
+export interface TabItem {
+  label: string;
+  panel: ReactNode;
+}
+
+export interface TabViewProps {
+  tabs: TabItem[];
+}
+
+export const TabView: FC<TabViewProps> = ({ tabs }) => {
+  return (
+    <Tab.Group>
+      <Tab.List className={classNames('tw-flex tw-w-full tw-border-b')}>
+        {tabs.map((tab) => (
+          <Tab key={tab.label} className={({ selected }) => classNames('tw-h-12 tw-flex-1')}>
+            {tab.label}
+          </Tab>
+        ))}
+      </Tab.List>
+      <Tab.Panels className="">
+        {tabs.map((tab) => (
+          <Tab.Panel key={tab.label} className={classNames()}>
+            {tab.panel}
+          </Tab.Panel>
+        ))}
+      </Tab.Panels>
+    </Tab.Group>
+  );
+};
